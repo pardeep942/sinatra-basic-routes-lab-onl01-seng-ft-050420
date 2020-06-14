e0dc33158194cc656a49ac8e62e81abc9555abd5
PARDEEP KAUR <pkaur84@aol.com>
2020-06-14 07:07:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,27 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
 end
+
+  get '/name' do
+    "My name is pari"
+end
+
+  get '/hometown' do
+    "My hometown is New York"
+end
+
+  get '/favorite-song' do
+    "My favorite song is punjabi"
+  end
+    
+end
+
+
+
+
+
+
+
